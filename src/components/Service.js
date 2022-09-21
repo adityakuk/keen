0c1e6c6227e6de5f8f7341f710b5663b889ba569
adityakuk <singhaditya2869@gmail.com>
2022-09-21 15:24:29 +0530

    initial commit

---
+++ src/components/Service.js
@@ -0,0 +1,56 @@
+const Service = () => {
+  return (
+    <div>
+      <section id="services" className="services">
+        <div className="container">
+
+          <div className="section-title">
+            <h2>Services</h2>
+            <h3>We do offer awesome <span>Services</span></h3>
+            <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
+          </div>
+
+          <div className="row">
+            <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+              <div className="icon-box">
+                <div className="icon"><i className="bx bxl-dribbble"></i></div>
+                <h4 className="title"><a href="">Lorem Ipsum</a></h4>
+                <p className="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+              </div>
+            </div>
+
+            <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+              <div className="icon-box">
+                <div className="icon"><i className="bx bx-file"></i></div>
+                <h4 className="title"><a href="">Sed ut perspiciatis</a></h4>
+                <p className="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+              </div>
+            </div>
+
+            <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+              <div className="icon-box">
+                <div className="icon"><i className="bx bx-tachometer"></i></div>
+                <h4 className="title"><a href="">Magni Dolores</a></h4>
+                <p className="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+              </div>
+            </div>
+
+            <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+              <div className="icon-box">
+                <div className="icon"><i className="bx bx-world"></i></div>
+                <h4 className="title"><a href="">Nemo Enim</a></h4>
+                <p className="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+              </div>
+
+              
+            </div>
+            
+          </div>
+        </div>
+        
+      </section>
+    </div>
+
+  )
+}
+export default Service
---
+++ src/components/Team.js
@@ -0,0 +1,143 @@
+const Team = () => {
+  return (
+    <div>
+      <section id="team" className="team">
+        <div className="container">
+          <div className="section-title">
+            <h2>Team</h2>
+            <h3>
+              Our Not Hardworking <span>Team</span>
+            </h3>
+            <p>
+              Ut possimus qui ut temporibus culpa velit eveniet modi omnis est
+              adipisci expedita at voluptas atque vitae autem.
+            </p>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-3 col-md-6 d-flex align-items-stretch">
+              <div className="member">
+                <div className="member-img">
+                  <img
+                    src="assets/img/team/team-1.jpg"
+                    className="img-fluid"
+                    alt=""
+                  />
+                  <div className="social">
+                    <a href="">
+                      <i className="bi bi-twitter"></i>
+                    </a>
+                    <a href="">
+                      <i className="bi bi-facebook"></i>
+                    </a>
+                    <a href="">
+                      <i className="bi bi-instagram"></i>
+                    </a>
+                    <a href="">
+                      <i className="bi bi-linkedin"></i>
+                    </a>
+                  </div>
+                </div>
+                <div className="member-info">
+                  <h4>Walter White</h4>
+                  <span>Chief Executive Officer</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6 d-flex align-items-stretch">
+              <div className="member">
+                <div className="member-img">
+                  <img
+                    src="assets/img/team/team-2.jpg"
+                    className="img-fluid"
+                    alt=""
+                  />
+                  <div className="social">
+                    <a href="">
+                      <i className="bi bi-twitter"></i>
+                    </a>
+                    <a href="">
+                      <i className="bi bi-facebook"></i>
+                    </a>
+                    <a href="">
+                      <i className="bi bi-instagram"></i>
+                    </a>
+                    <a href="">
+                      <i className="bi bi-linkedin"></i>
+                    </a>
+                  </div>
+                </div>
+                <div className="member-info">
+                  <h4>Sarah Jhonson</h4>
+                  <span>Product Manager</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6 d-flex align-items-stretch">
+              <div className="member">
+                <div className="member-img">
+                  <img
+                    src="assets/img/team/team-3.jpg"
+                    className="img-fluid"
+                    alt=""
+                  />
+                  <div className="social">
+                    <a href="">
+                      <i className="bi bi-twitter"></i>
+                    </a>
+                    <a href="">
+                      <i className="bi bi-facebook"></i>
+                    </a>
+                    <a href="">
+                      <i className="bi bi-instagram"></i>
+                    </a>
+                    <a href="">
+                      <i className="bi bi-linkedin"></i>
+                    </a>
+                  </div>
+                </div>
+                <div className="member-info">
+                  <h4>William Anderson</h4>
+                  <span>CTO</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6 d-flex align-items-stretch">
+              <div className="member">
+                <div className="member-img">
+                  <img
+                    src="assets/img/team/team-4.jpg"
+                    className="img-fluid"
+                    alt=""
+                  />
+                  <div className="social">
+                    <a href="">
+                      <i className="bi bi-twitter"></i>
+                    </a>
+                    <a href="">
+                      <i className="bi bi-facebook"></i>
+                    </a>
+                    <a href="">
+                      <i className="bi bi-instagram"></i>
+                    </a>
+                    <a href="">
+                      <i className="bi bi-linkedin"></i>
+                    </a>
+                  </div>
+                </div>
+                <div className="member-info">
+                  <h4>Amanda Jepson</h4>
+                  <span>Accountant</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+export default Team;
