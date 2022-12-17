a987398aaff673db35ad565dc6b369281f9f6c08
adityakuk <singhaditya2869@gmail.com>
2022-12-17 10:57:03 +0530

    react redux setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 // import Login from './components/auth/Login';
 import { Quora } from './components/Quora';
 import './App.css';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { Login } from '@mui/icons-material';
+import { selectUser } from './feature/userSlice';
 
 
 const App = () => {
-  // const user = useSelector((store)=>store)
+  // const user = useSelector((store) => store)
+  const user = useSelector(selectUser)
   return (
     <div className="App">
-      <Quora />
+      {
+        user ? (<Quora />) : (<Login />)
+      }
+
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,22 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import TimeAgo from 'javascript-time-ago'
+import reportWebVitals from './reportWebVitals';
 
+import TimeAgo from 'javascript-time-ago'
 // English
 import en from 'javascript-time-ago/locale/en.json'
-TimeAgo.addDefaultLocale(en)
-
+import { Provider } from 'react-redux';
+import store from './app/store';
 // Create formatter (English).
 // const timeAgo = new TimeAgo('en-US')
-
+TimeAgo.addDefaultLocale(en)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    
-        <App />
-    
+        <React.StrictMode>
+                <Provider store={store}>
+                        <App />
+                </Provider>
+        </React.StrictMode>,
 );
+
+// document.getElementById("root")
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
