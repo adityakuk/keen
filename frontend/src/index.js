9fe7eb52282d9c765e9b22c27169c6788c9dc230
Aditya <singhaditya2869@gmail.com>
2024-12-28 10:29:39 +0530

    some changes for setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
 
 import TimeAgo from 'javascript-time-ago'
 // English
 import en from 'javascript-time-ago/locale/en.json'
-import { Provider } from 'react-redux';
-import store from './app/store';
+import { Provider } from 'react-redux'
+import store from './app/store'
 // Create formatter (English).
 // const timeAgo = new TimeAgo('en-US')
 TimeAgo.addDefaultLocale(en)
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-        <React.StrictMode>
-                <Provider store={store}>
-                        <App />
-                </Provider>
-        </React.StrictMode>,
-);
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+)
+
+// update somthing
 
 // document.getElementById("root")
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
